9582b3061d03854448ad6af5c57f3ff2f09257f1
MichaelChan2417 <45192104+MichaelChan2417@users.noreply.github.com>
2022-06-17 22:29:16 +0800

    Update shows.c

--- Chapt. 2/shows.c
+++ Chapt. 2/shows.c
@@ -12,7 +12,17 @@ void show_bytes(byte_pointer start, int len){
     printf("\n");
 }
 
+void show_int(int x){
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
 
+void show_float(float x){
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x){
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
 
 int main(){
     char s = 'a';
